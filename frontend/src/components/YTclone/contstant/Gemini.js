12b839533050a8b918f09feffdbc3c6dac19613a
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2024-12-11 19:09:43 +0530

    protect API key

--- frontend/src/components/YTclone/contstant/Gemini.js
+++ frontend/src/components/YTclone/contstant/Gemini.js
@@ -1,1 +1,5 @@
-export const API_Gemini = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyD0o5aN82ZmXlyoR1wn9NQ2S49bh0vfWYc"
+
+export const API_Gemini = import.meta.env.VITE_GEMINI_API_KEY;
+
+
+
